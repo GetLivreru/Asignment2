0365b90fbdfe749ac65e7bcb00ea0d6eea7b3b27
theadilet <110306539+GetLivreru@users.noreply.github.com>
2023-04-14 23:00:13 +0600

    Update MyArrayList.java

--- MyArrayList.java
+++ MyArrayList.java
@@ -1,7 +1,20 @@
 import java.util.*;
-public class MyArrayList<E> {
+public class MyArrayList<E> implements List<E>{
+  private Object[] elements;
+  private int size;
+  
+public MyArraylist(){
+  // This is an implementation of a custom ArrayList called MyArrayList that implements the List interface.
+  elements = new Object[10];
+  size = 0;
+}
+ private void resize() {
+        int newSize = elements.length * 2;
+        elements = Arrays.copyOf(elements, newSize);
+    }
 
-public class main{
-  public static void main (String[] args){
-  }
+ // Testing the implementation
+    public static void main(String[] args) {
+        
+ }
 }
