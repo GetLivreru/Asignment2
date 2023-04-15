761b119a1dcf6a83216fea31c2a42a1ab5814abd
theadilet <110306539+GetLivreru@users.noreply.github.com>
2023-04-15 23:39:47 +0600

    Update MyLinkedList.java

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -1,6 +1,21 @@
-public class Main
-{
-	public static void main(String[] args) {
-		System.out.println("Hello World");
-	}
-}
+public class MyLinkedList<T> {
+    private static class Node<T> {
+        T data;
+        Node<T> prev;
+        Node<T> next;
+        public Node(T data) {
+            this.data = data;
+            this.prev = null;
+            this.next = null;
+        }
+    }
+
+    private Node<T> head;
+    private Node<T> tail;
+    private int size;
+
+    public MyLinkedList() {
+        head = null;
+        tail = null;
+        size = 0;
+    }
