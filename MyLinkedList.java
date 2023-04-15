faf258417bbfdc62425ce97a595c7b2f6b43835c
theadilet <110306539+GetLivreru@users.noreply.github.com>
2023-04-15 23:40:16 +0600

    Update MyLinkedList.java

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -19,3 +19,11 @@ public class MyLinkedList<T> {
         tail = null;
         size = 0;
     }
+    public int size() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+}
